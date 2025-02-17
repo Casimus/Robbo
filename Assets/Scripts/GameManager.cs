09c666df78e8e2c0c6a37888d786b9e72a6e4b4a
Grzegorz Kazimierczak <grzegorz.kazimierczak@wp.pl>
2025-02-17 16:28:05 +0100

    po 20 lekcji

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
 
     [SerializeField] private float worldSpeed = 0.2f;
+    [SerializeField] private Button resetButton;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    
+    private float score = 0;
+    bool isAlive = true;
 
     private void Awake()
     {
@@ -21,6 +27,40 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        resetButton.gameObject.SetActive(false);
+        score = 0;
+        isAlive = true;
+    }
+
+    private void Update()
+    {
+        if (isAlive)
+        {
+            score += Time.deltaTime;
+            scoreText.text = $"Score: {score:F0}";
+        }
+    }
+
+    public void SetAlive(bool alive)
+    {
+        isAlive = alive;
+    }
+
+    public void ShowButton()
+    {
+        resetButton.gameObject.SetActive(true);
+    }
+
     public float GetWorldSpeed() => worldSpeed;
     
+    public void SetWorldSpeed(float speed)
+    {
+        worldSpeed = speed;
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,35 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
+
 
 public class Player : MonoBehaviour
 {
     
-    
-
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Obstacle")
         {
-            SceneManager.LoadScene(0);
-            Debug.Log("Game Over");
-            
+            GameOver();
         }
+    }
+
+    private void GameOver()
+    {
+        SetGameOverUI();
+        GetComponent<PlayerController>().enabled = false;
+        SetGameOverRigidbody();
+    }
+
+    private void SetGameOverRigidbody()
+    {
+        var rb = GetComponent<Rigidbody2D>();
+        rb.velocity = Vector2.zero;
+        rb.gravityScale = 0;
+    }
+
+    private static void SetGameOverUI()
+    {
+        GameManager.Instance.SetWorldSpeed(0);
+        GameManager.Instance.ShowButton();
+        GameManager.Instance.SetAlive(false);
     }
 }
